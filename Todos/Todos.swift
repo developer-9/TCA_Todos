dd45b8d4ec2d396fe5c7568624465ab57a9894d3
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-05-06 11:53:31 +0900

    Add State

--- Todos/Todos.swift
+++ Todos/Todos.swift
@@ -8,3 +8,25 @@
 import ComposableArchitecture
 import SwiftUI
 
+enum Filter: LocalizedStringKey, CaseIterable, Hashable {
+    case all = "All"
+    case active = "Active"
+    case completed = "Completed"
+}
+
+struct AppState: Equatable {
+    var editMode: EditMode = .inactive
+    var filter: Filter = .all
+    var todos: IdentifiedArrayOf<Todo> = []
+    
+    var filteredTodos: IdentifiedArrayOf<Todo> {
+        switch filter {
+        case .all:
+            return self.todos
+        case .active:
+            return self.todos.filter { !$0.isComplete }
+        case .completed:
+            return self.todos.filter(\.isComplete)
+        }
+    }
+}
