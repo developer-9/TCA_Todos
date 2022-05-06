14222760b1b87b2e8601351b1b3c89e3eb3437c8
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-05-06 11:06:57 +0900

    Add State

---
+++ Todos/Todo.swift
@@ -0,0 +1,18 @@
+//
+//  Todo.swift
+//  Todos
+//
+//  Created by Taisei Sakamoto on 2022/05/06.
+//
+
+import Foundation
+import ComposableArchitecture
+
+
+//MARK: - State
+
+struct Todo: Equatable, Identifiable {
+    var description = ""
+    let id: UUID
+    var isComplete = false
+}
