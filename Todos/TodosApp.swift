e1d83accf231e6e719d8b624c2e27e9b37b154fb
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-05-06 10:25:38 +0900

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Taisei Sakamoto on 2022/05/06.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
